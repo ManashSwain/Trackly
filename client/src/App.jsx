ae0789660ced2d7ca9609f4b4b14df3c23858b59
ManashSwain <manash0802@gmail.com>
2025-08-30 11:45:58 +0530

    feat: implement react-router-dom setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
 
 function App() {
 
   return (
     <>
-     <p>App component</p>
+    <RouterProvider router={router} />
     </>
   )
 }
