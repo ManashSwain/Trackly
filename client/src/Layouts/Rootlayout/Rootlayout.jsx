955c478810125a499c8ed2b740f2dcfbf464a3dc
ManashSwain <manash0802@gmail.com>
2025-08-31 11:40:26 +0530

    feat: implement sidebar ui design fixes

--- client/src/Layouts/Rootlayout/Rootlayout.jsx
+++ client/src/Layouts/Rootlayout/Rootlayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
+import Sidebar from '../../components/Sidebar/Sidebar'
 
 
 const Rootlayout = () => {
   return (
     <>
     <Navbar/>
+    <Sidebar/>
     <Footer/>
     </>
   )
