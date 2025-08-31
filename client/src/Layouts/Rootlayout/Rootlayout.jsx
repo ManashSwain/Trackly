2b4f3b7fa615fd551a961aba0b82ff584ba3ad03
ManashSwain <manash0802@gmail.com>
2025-08-31 13:00:52 +0530

    feat: implement page structure

--- client/src/Layouts/Rootlayout/Rootlayout.jsx
+++ client/src/Layouts/Rootlayout/Rootlayout.jsx
@@ -10,7 +10,7 @@ const Rootlayout = () => {
     <>
     <Navbar/>
     <Sidebar/>
-     <Outlet />
+    <Outlet />
     <Footer/>
     </>
   )
---
+++ client/src/Pages/Createtask/Createtask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Createtask = () => {
+  return (
+  <>
+  <h1>Create Task</h1>
+  </>
+  )
+}
+
+export default Createtask
