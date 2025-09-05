6b93b7de8d0cc959a82266478ac5c8df96e5d04e
ManashSwain <manash0802@gmail.com>
2025-09-05 12:50:35 +0530

    feat: implement dashboard ui design

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../../components/Hero/Hero'
 
 const Dashboard = () => {
   return (
     <>
     <h1 className='text-red-800'>Dashboard page</h1>
+    <Hero/>
     </>
   )
 }
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -3,28 +3,31 @@ import React from 'react'
 const Hero = () => {
   return (
  <>
-  <section>
+  <section className='flex flex-col w-[80vw] justify-between bg-gray-100 p-5 rounded-lg m-5'>
     <div>
-        <h2>Hello! Mike</h2>
-        <p>Tuesday 25 March 2025</p>
+        <h2 className='text-black text-3xl'>Hello! Mike</h2>
+        <p className='text-gray-500'>Tuesday 25 March 2025</p>
     </div>
-    <div className='m-5'>
+    <div className='mt-8 flex justify-between'>
       {/* Total Tasks  */}
-      <div >
-        <ul>
-            <li className='marker:text-blue-700'>
-                <span>18</span>
-                <span>Total tasks</span>
-            </li>
+      <div  >
+        <ul className='flex gap-2 items-center'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-gallery-horizontal-end-icon lucide-gallery-horizontal-end text-blue-700"><path d="M2 7v10"/><path d="M6 5v14"/><rect width="12" height="18" x="10" y="3" rx="2"/></svg>
+              <div>
+                <span className='font-bold mx-1'>18</span>
+                <span className='text-gray-500 font-medium'>Total tasks</span>
+              </div>
         </ul>
       </div>
        {/* Pending Tasks  */}
       <div>
         <ul>
-            <li className='marker:text-purple-700'>
-                <span>11</span>
-                <span>Pending tasks</span>
-            </li>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-clipboard-clock-icon lucide-clipboard-clock text-purple-700"><path d="M16 14v2.2l1.6 1"/><path d="M16 4h2a2 2 0 0 1 2 2v.832"/><path d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h2"/><circle cx="16" cy="16" r="6"/><rect x="8" y="2" width="8" height="4" rx="1"/></svg>
+              <div>
+                 <span className='font-bold mx-1'>11</span>
+                <span className='text-gray-500 font-medium'>Pending tasks</span>
+              </div>
+               
         </ul>
       </div>
        {/* In progress Tasks  */}
