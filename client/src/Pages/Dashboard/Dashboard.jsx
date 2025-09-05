352d7e8cf201c0ac762b29f33a5e9acea4bef309
ManashSwain <manash0802@gmail.com>
2025-09-05 17:38:23 +0530

    feat: implement piechart component

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
+import Distribution from '../../components/Distribution/Distribution'
 
 const Dashboard = () => {
   return (
     <>
      <div className='inline-block'>
       <Hero/>
+      <Distribution/>
      </div>
       
     </>
---
+++ client/src/components/Distribution/Distribution.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { PieChart } from '@mui/x-charts/PieChart';
+
+
+const Distribution = () => {
+  return (
+    <>
+    <PieChart
+      series={[
+        {
+          data: [
+            { id: 0, value: 10, label: 'series A' },
+            { id: 1, value: 7, label: 'series B' },
+            { id: 2, value: 20, label: 'series C' },
+          ],
+        },
+      ]}
+      width={200}
+      height={200}
+    />
+    </>
+  )
+}
+
+export default Distribution
