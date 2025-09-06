3e90861c482dfcdc3c37790d702aec3ef244d1cd
ManashSwain <manash0802@gmail.com>
2025-09-06 13:40:09 +0530

    feat: implement datepicker ui design

--- client/src/Pages/Createtask/Createtask.jsx
+++ client/src/Pages/Createtask/Createtask.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Createindividualtask from '../../components/CreateIndividualtask/Createindividualtask'
 
 const Createtask = () => {
   return (
   <>
   <h1>Create Task</h1>
+  <section className='inline-block p-5'>
+    <Createindividualtask/>
+  </section>
   </>
   )
 }
---
+++ client/src/components/CreateIndividualtask/Createindividualtask.jsx
@@ -0,0 +1,195 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import { ChevronLeft, ChevronRight, Calendar } from "lucide-react";
+import "react-datepicker/dist/react-datepicker.css";
+
+const CustomHeader = ({
+  date,
+  decreaseMonth,
+  increaseMonth,
+  prevMonthButtonDisabled,
+  nextMonthButtonDisabled,
+}) => (
+  <div className="flex items-center justify-between px-2 py-1">
+    <button
+      onClick={decreaseMonth}
+      disabled={prevMonthButtonDisabled}
+      type="button"
+      className="p-1"
+    >
+      <ChevronLeft className="w-5 h-5 text-blue-600" />
+    </button>
+    <span className="font-semibold">
+      {date.toLocaleString("default", { month: "long", year: "numeric" })}
+    </span>
+    <button
+      onClick={increaseMonth}
+      disabled={nextMonthButtonDisabled}
+      type="button"
+      className="p-1"
+    >
+      <ChevronRight className="w-5 h-5 text-blue-600" />
+    </button>
+  </div>
+);
+
+const Createindividualtask = () => {
+  const [startDate, setStartDate] = useState(new Date());
+
+  return (
+    <>
+      <section>
+        <form>
+          <div className="space-y-0">
+            <div className=" border-gray-900/10 ">
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-4">
+                  <label htmlFor="username" className="block text-sm/6 font-medium text-gray-900">
+                    Task Title
+                  </label>
+                  <div className="mt-2">
+                    <div className="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                      <div className="shrink-0 select-none text-base text-gray-500 sm:text-sm/6">
+                        workcation.com/
+                      </div>
+                      <input
+                        id="username"
+                        type="text"
+                        name="username"
+                        placeholder="janesmith"
+                        className="block min-w-0 grow bg-white py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-full">
+                  <label htmlFor="about" className="block text-sm/6 font-medium text-gray-900">
+                    Task Description
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="about"
+                      name="about"
+                      rows="3"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="border-b border-gray-900/10 pb-12">
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-3">
+                  <label htmlFor="country" className="block text-sm/6 font-medium text-gray-900">
+                    Priority
+                  </label>
+                  <div className="mt-2 grid grid-cols-1">
+                    <select
+                      id="country"
+                      name="country"
+                      autoComplete="country-name"
+                      className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    >
+                      <option>Low</option>
+                      <option>Medium</option>
+                      <option>High</option>
+                    </select>
+                    <svg
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      data-slot="icon"
+                      aria-hidden="true"
+                      className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                    >
+                      <path
+                        d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+
+                <div className="col-span-full">
+                  <label htmlFor="street-address" className="block text-sm/6 font-medium text-gray-900">
+                    Due Date
+                  </label>
+                  <div className="relative max-w-sm">
+                    {/* Calendar icon inside the input, before the date */}
+                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-calendar-icon lucide-calendar relative  text-gray-600 top-8 left-3 z-50"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/></svg>
+                    <DatePicker
+                      selected={startDate}
+                      onChange={(date) => setStartDate(date)}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 z-10"
+                      placeholderText="Select date"
+                      calendarClassName="!z-50"
+                      renderCustomHeader={(props) => <CustomHeader {...props} />}
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2 sm:col-start-1">
+                  <label htmlFor="city" className="block text-sm/6 font-medium text-gray-900">
+                    City
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="city"
+                      type="text"
+                      name="city"
+                      autoComplete="address-level2"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                  <label htmlFor="region" className="block text-sm/6 font-medium text-gray-900">
+                    State / Province
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="region"
+                      type="text"
+                      name="region"
+                      autoComplete="address-level1"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                  <label htmlFor="postal-code" className="block text-sm/6 font-medium text-gray-900">
+                    ZIP / Postal code
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="postal-code"
+                      type="text"
+                      name="postal-code"
+                      autoComplete="postal-code"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button type="button" className="text-sm/6 font-semibold text-gray-900">
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Createindividualtask;
