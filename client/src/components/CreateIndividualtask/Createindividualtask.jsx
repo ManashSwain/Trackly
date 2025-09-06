158b702823df00e0caa1539a0a99b7c0858472ee
ManashSwain <manash0802@gmail.com>
2025-09-06 14:01:19 +0530

    feat: implement todo ui design

--- client/src/components/CreateIndividualtask/Createindividualtask.jsx
+++ client/src/components/CreateIndividualtask/Createindividualtask.jsx
@@ -139,38 +139,22 @@ const Createindividualtask = () => {
                       autoComplete="address-level2"
                       className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                     />
-                  </div>
+                  </div>  
                 </div>
 
-                <div className="sm:col-span-2">
-                  <label htmlFor="region" className="block text-sm/6 font-medium text-gray-900">
-                    State / Province
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="region"
-                      type="text"
-                      name="region"
-                      autoComplete="address-level1"
-                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                    />
-                  </div>
-                </div>
+                  <div class="col-span-full">
+          <label for="street-address" class="block text-sm/6 font-medium text-gray-900">Todo checklist</label>
+          <div class="mt-2 flex gap-x-3">
+            <input id="street-address" type="text" name="street-address" autocomplete="street-address" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+             <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Add
+            </button>
+          </div>
+        </div>
 
-                <div className="sm:col-span-2">
-                  <label htmlFor="postal-code" className="block text-sm/6 font-medium text-gray-900">
-                    ZIP / Postal code
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="postal-code"
-                      type="text"
-                      name="postal-code"
-                      autoComplete="postal-code"
-                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                    />
-                  </div>
-                </div>
               </div>
             </div>
           </div>
