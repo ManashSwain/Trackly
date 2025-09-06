4c9eda0ccef5a5f8f788a400f4ad162f554b61d7
ManashSwain <manash0802@gmail.com>
2025-09-06 23:31:23 +0530

    feat: implement profile page ui design

--- client/src/components/Profiledetails/Profiledetails.jsx
+++ client/src/components/Profiledetails/Profiledetails.jsx
@@ -55,20 +55,28 @@ const Profiledetails = () => {
         </div>
       </div>
     </div>
-    <div className="border-b border-gray-900/10 pb-12">
-     
-      <div className="mt-10 space-y-10">
- 
-      </div>
-    </div>
   </div>
-  <div className="mt-6 flex items-center justify-end gap-x-6">
+  <div className='border-b border-gray-900/10 pb-12 -mt-8'>
+    <h2 className="text-base/7 font-semibold text-red-700">Update Account Password</h2>
+      <div className="sm:col-span-4 mt-2">
+          <label for="email" className="block text-sm/6 font-medium text-gray-900">Update Password</label>
+          <div className="mt-2">
+            <input id="email" type="email" name="email" autocomplete="email" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          </div>
+        </div>
+         <div className="sm:col-span-4">
+          <label for="email" className="block text-sm/6 font-medium text-gray-900">Re-enter your password</label>
+          <div className="mt-2">
+            <input id="email" type="email" name="email" autocomplete="email" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          </div>
+        </div>
+        <button type="submit" className="mt-4 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update Password</button>
+  </div>
+  <div className="mt-2 flex items-center justify-end gap-x-6">
     <button type="button" className="text-sm/6 font-semibold text-gray-900">Cancel</button>
     <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
   </div>
 </form>
-
-
    </>
   )
 }
