d9a11d15ad65defe4469e7d5643a9642bcc0fae5
ManashSwain <manash0802@gmail.com>
2025-09-07 18:01:44 +0530

    feat: implement profile structure

--- client/src/components/Profiledetails/Profiledetails.jsx
+++ client/src/components/Profiledetails/Profiledetails.jsx
@@ -5,10 +5,10 @@ const Profiledetails = () => {
    <>
    <form>
     <div className="border-gray-900/10 pb-12">
-      <h2 className="text-base/7 font-semibold text-gray-900">Profile Details</h2>
-       <p className="mt-1 text-sm/6 text-gray-600">This information will be visible to others, so please be mindful of what you share.</p>
+      <h2 className="font-semibold text-2xl text-gray-600">Profile Details</h2>
+       <p className="mt-2 text-sm/6 text-gray-600">This information will be visible to others, so please be mindful of what you share.</p>
     <div className="border-b border-gray-900/10 pb-12 mt-2">
-      <h2 className="text-base/7 font-semibold text-gray-900">Personal Information</h2>
+      <h2 className="text-xl font-semibold text-gray-900">Personal Information</h2>
       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div className="sm:col-span-3">
           <label for="first-name" className="block text-sm/6 font-medium text-gray-900">First name</label>
@@ -56,9 +56,9 @@ const Profiledetails = () => {
     </div>
   </div>
   <div className='border-b border-gray-900/10 pb-12 -mt-8'>
-    <h2 className="text-base/7 font-semibold text-red-700">Update Account Password</h2>
+    <h2 className="text-xl font-semibold text-red-700">Update Password</h2>
       <div className="sm:col-span-4 mt-2">
-          <label for="email" className="block text-sm/6 font-medium text-gray-900">Update Password</label>
+          <label for="email" className="block text-sm/6 font-medium text-gray-900">Enter your Password</label>
           <div className="mt-2">
             <input id="email" type="email" name="email" autocomplete="email" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
           </div>
@@ -70,7 +70,11 @@ const Profiledetails = () => {
           </div>
         </div>
         <button type="submit" className="mt-4 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update Password</button>
+        <p className='mt-2 text-yellow-600'>⚠️ If you lose your password, your account cannot be recovered. Please  <span className='text-blue-600 underline cursor-pointer'>download</span>  and securely store your password to keep your access safe.</p>
   </div>
+  <h2 className="text-xl font-semibold text-red-700 mt-2">Delete Account</h2>
+   <p className="mt-2  text-gray-600">Once you delete your account, there is no going back. Please be certain. </p>
+  <button type="submit" className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 mt-4">Delete Account</button>
   <div className="mt-2 flex items-center justify-end gap-x-6">
     <button type="button" className="text-sm/6 font-semibold text-gray-900">Cancel</button>
     <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
