0bae0d5e298dd81d52bc6bbdb6f44ed23f8a18c7
ManashSwain <manash0802@gmail.com>
2025-09-14 10:51:14 +0530

    feat: implement dashboard design changes

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -9,8 +9,16 @@ const Dashboard = () => {
     <>
      <div className='inline-block'>
       <Hero/>
-      <Distribution/>
-      <Prioritychart/>
+      <section className='flex justify-between items-center p-5 gap-2'>
+        <div className='w-[28vw] h-[50vh] outline-double outline-1 outline-gray-300 p-5 rounded-lg'>
+          <h1 className='font-bold text-black mb-10 text-xl'>Task Distribution</h1>
+        <Distribution/>
+        </div>
+        <div className='w-[48vw] h-[50vh] outline-double outline-1 outline-gray-300 p-5 rounded-lg'>
+           <h1 className='font-bold text-black mb-6 text-xl'>Task Priority Levels</h1>
+         <Prioritychart/>
+        </div>
+      </section>
       <Datagrid/>
      </div>
       
