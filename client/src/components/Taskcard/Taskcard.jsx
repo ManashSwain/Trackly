77b71554021e922fb2629c9d273908a53bd5d12b
ManashSwain <manash0802@gmail.com>
2025-09-14 22:59:57 +0530

    feat: implement taskcard ui design

--- client/src/components/Taskcard/Taskcard.jsx
+++ client/src/components/Taskcard/Taskcard.jsx
@@ -9,10 +9,10 @@ const Taskcard = () => {
           href="#"
           class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100"
         >
-            <div>
+            <div className="m-2">
           <button
             type="button"
-            class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 "
+            className="px-3 py-2 text-xs font-medium text-center mr-4 text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 "
           >
             Extra small
           </button>
@@ -31,15 +31,25 @@ const Taskcard = () => {
             far, in reverse chronological order.
           </p>
            <div>
-          <p>
-            Task Done <span>2/5</span>
+          <p className="text-sm mt-2">
+            Task Done :  <span className="font-bold">2/5</span>
           </p>
           <div class="w-full bg-gray-200 rounded-full h-2.5">
             <div
-              class="bg-blue-600 h-2.5 rounded-full"
+              className="bg-blue-600 h-2.5 rounded-full mt-2"
               style={{ width: "45%" }}
             ></div>
           </div>
+        </div>
+        <div className="flex justify-between mt-4">
+            <div>
+                <p className="text-gray-500 text-sm">Start Date</p>
+                <p className="font-bold text-sm">27/06/20205</p>
+            </div>
+            <div>
+                <p className="text-gray-500 text-sm">End Date</p>
+                <p className="font-bold text-sm">27/06/20205</p>
+            </div>
         </div>
         </a>
       </div>
